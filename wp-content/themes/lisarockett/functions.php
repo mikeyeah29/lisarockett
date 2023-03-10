56352e48c4438e054f9d07401f585e1c01f0ace0
mikeyeah29 <mikerockett@live.com>
2023-03-10 11:20:57 +0000

    removed admin bar heght

--- wp-content/themes/lisarockett/functions.php
+++ wp-content/themes/lisarockett/functions.php
@@ -84,4 +84,9 @@ function dd($var)
     die();
 }
 
+function lr_remove_admin_login_header() {
+    remove_action('wp_head', '_admin_bar_bump_cb');
+}
+add_action('get_header', 'lr_remove_admin_login_header');
+
 ?>
